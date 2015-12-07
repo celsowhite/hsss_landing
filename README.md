b0c57ac8b4a7215191eb1ef7b44dfdf1a98a15a1
celsowhite <celso.white13@gmail.com>
2015-12-07 16:26:18 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# He Said She Said Landing
+
+Development of the project landing page.
---
+++ app/config.rb
@@ -0,0 +1,11 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "img"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+line_comments = false
+relative_assets = true
+
+output_style = :compressed
+environment = :development
