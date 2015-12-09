edfdf7de3d0c03f056c30d28880b763bcb7b5387
celsowhite <celso.white13@gmail.com>
2015-12-09 14:50:01 -0500

    Perfect section navigation arrows.

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,19 @@
 $(document).ready(function(){
 
 	$('#main_content').fullpage({
-		navigation: true,
+		navigation: false,
+	});
+
+	$('#move_down').click(function() {
+
+		$.fn.fullpage.moveSectionDown();
+
+	});
+
+	$('#move_up').click(function() {
+
+		$.fn.fullpage.moveSectionUp();
+
 	});
 
 });
