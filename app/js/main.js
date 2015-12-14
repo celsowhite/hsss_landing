fcf06d73d6268edbd2896f4eb6f0346e7a047d22
Matt Watkajtys <watkajtys@gmail.com>
2015-12-14 03:05:36 -0500

    Shifting slider logic out of main

--- app/js/main.js
+++ app/js/main.js
@@ -53,32 +53,4 @@ $(document).ready(function() {
 		console.log('yes');
 
 	});
-
-
-	var $element = $('input[type="range"]');
-	var $output = $('output');
-
-	function updateOutput(el, val) {
-		el.textContent = val;
-		var numerical = parseInt(val);
-		console.log('update');
-		//0 - 3 | 4 - 6 | 7 - 10
-		if (numerical <= 3) {
-			el.textContent = 'they mean less than they say'
-		} else if (numerical > 3 && numerical <= 6) {
-			el.textContent = 'they mean what they say';
-		} else {
-			el.textContent = 'they mean more than they say';
-		}
-	}
-
-	$element.rangeslider({
-		polyfill: false,
-		onInit  : function () {
-			updateOutput($output[0], this.value);
-		}
-	})
-		.on('input', function () {
-			updateOutput($output[0], this.value);
-		});
 });
